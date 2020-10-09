eca9ac53125d96812c8114d1f414dda2d8ff4826
cassfan1 <WFAN@beckman.com>
2020-10-09 16:59:31 +0800

    update

--- Azure.IoTHub.Executer/Program.cs
+++ Azure.IoTHub.Executer/Program.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.Azure.Devices.Client;
 using Newtonsoft.Json;
 using System;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client.Transport;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace Azure.IoTHub.Executer
 {
@@ -12,7 +16,16 @@ namespace Azure.IoTHub.Executer
 
     static async Task Main(string[] args)
     {
-      DeviceClient deviceClient = DeviceClient.CreateFromConnectionString(DeviceConnectionString);
+      await UploadFileAsync();
+
+      //await SyncFileAsync();
+    }
+
+    private static async Task SyncFileAsync()
+    {
+      // connect to iothub
+      var deviceClient = DeviceClient.CreateFromConnectionString(DeviceConnectionString);
+
       while (true)
       {
         var msgModel = new
@@ -27,6 +40,40 @@ namespace Azure.IoTHub.Executer
 
         await Task.Delay(1000);
       }
+    }
+
+    private static async Task UploadFileAsync()
+    {
+      // use blob sdk to upload with blob uri
+      string fileName = "cytoflex123.txt";
+      string filePath = "C://Cass//cyto_flex_log.txt";
+
+      // connect to IoTHub
+      var deviceClient = DeviceClient.CreateFromConnectionString(DeviceConnectionString);
+
+      // get linked blob sas uri
+      var blob = await deviceClient.GetFileUploadSasUriAsync(new FileUploadSasUriRequest { BlobName = fileName });
+      var blobUri = $"https://{blob.HostName}/{blob.ContainerName}/{blob.BlobName}{blob.SasToken}";
+
+      // call blob sdk to upload files
+      var destinationBlob = new CloudBlockBlob(new Uri(blobUri));
+      await destinationBlob.UploadFromFileAsync(filePath);
+
+
+      //CloudStorageAccount cloudStorageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=casstestlake;AccountKey=BFhOE3ZqkeM55Cbpl2d9MsYQWM3jV4k0i8XoZPgL5M62APPUWy7s1IbGSV7WIgQqkjd4vj8YqJdtBbDMKZanbQ==;EndpointSuffix=core.windows.net");
+      //CloudBlobClient cloudBlobClient = cloudStorageAccount.CreateCloudBlobClient();
+      //CloudBlobContainer cloudBlobContainer = cloudBlobClient.GetContainerReference(containerName);
+
+      //if (await cloudBlobContainer.CreateIfNotExistsAsync())
+      //{
+      //  await cloudBlobContainer.SetPermissionsAsync(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
+      //}
+
+      //CloudBlockBlob cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(fileName);
+      //string blobUri = cloudBlockBlob.Uri.OriginalString;
+
+      //var cbBlob = new CloudBlockBlob(cloudBlockBlob.Uri);
+      //await cloudBlockBlob.UploadFromFileAsync(filePath);
     }
   }
 }
